21ffbc1c18f0ee976242190fa0560f950a189797
znznz6037 <znznz6037@gmail.com>
2026-03-01 19:27:14 +0900

    feature: Add User package for OAuth2.0

---
+++ src/main/java/com/psb/summerwiki_api/user/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.psb.summerwiki_api.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.psb.summerwiki_api.global.common.ApiResponse;
+import com.psb.summerwiki_api.user.dto.UserResponse;
+import com.psb.summerwiki_api.user.entity.User;
+import com.psb.summerwiki_api.user.repository.UserRepository;
+import com.psb.summerwiki_api.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ApiResponse<UserResponse> getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = (String) principal;
+
+        UserResponse userResponse = userService.getCurrentUser(email);
+        if (userResponse == null) {
+            return ApiResponse.error("User not found");
+        }
+
+        return ApiResponse.success(userResponse);
+    }
+    
+}
